d544a9609a92354769d1ae462ceca88f1eb79861
Sally <causeysally@gmail.com>
2025-10-14 22:07:07 -0500

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import { useState } from 'react'
-
 import './App.css'
+import Genre from './pages/Genre'
+import Ratings from './pages/Ratings'
+import data from './data.json'
+import Title from './pages/Title'
+console.log(data.Title)
+console.log(data.Genre)
+console.log(data.Ratings)
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState("home")
+
+  function titlePage() {
+    setCurrentPage("title")
+
+  }
+
+  function genrePage() {
+    setCurrentPage("genre")
+  }
+
+  function ratingsPage() {
+    setCurrentPage("ratings")
+  }
+
+  let pageContent = <Title data={data} />
+
+  if (currentPage === "home") {
+    pageContent = <Title data={data} />
+  } else if (currentPage === "genre") {
+    pageContent = <Genre data={data} />
+  } else if (currentPage === "ratings") {
+    pageContent = <Ratings data={data} />
+
+  }
 
   return (
     <>
-    
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+        <nav>
+          <h1>Netflix Movies</h1>
+          {pageContent}
+          <button type="button" onClick={titlePage}>Home</button>
+          <button type="button" onClick={genrePage}>Genre</button>
+          <button type="button" onClick={ratingsPage}>Ratings</button>
+        </nav>
+      </div>
+
     </>
   )
 }
---
+++ src/pages/Ratings.jsx
@@ -0,0 +1,9 @@
+export default function Ratings() {
+    return (
+        <>
+            <h1>Ratings</h1>
+            <p>Select a Rating</p>
+            
+        </>
+    )
+}
