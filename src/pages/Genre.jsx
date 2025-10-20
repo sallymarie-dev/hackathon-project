b6c5a77ffcfdf0abf78db74200452831124af4e5
Sally <causeysally@gmail.com>
2025-10-19 20:31:28 -0500

    pages display movie, title, ratings, and genre

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,47 +1,138 @@
-import { useState } from "react"
+// import { useState } from "react";
+
+// export default function Genre({ data }) {
+
+//     const [filteredList, setFilteredList] = useState([]);
+
+//     function handleGenre(genre) {
+//         console.log("clicked Genre")
+
+//         const comedyGenre = data.filter((movie)=> movie.Genre.includes(genre));
+
+//         setFilteredList(comedyGenre)
+//     }
+
+
+//     // for (let i = 0; i < comedyGenre.length; i++) {
+//     //     filteredList.push(<>
+//     //         <li>{filteredList[i].Name}</li>
+//     //     </>)
+//     // }
+
+
+// }
+
+
+
+
+// import { useState } from "react";
+
+// export default function Genre({ data }) {
+//   const [filteredList, setFilteredList] = useState([]);
+
+//   function handleGenre(genre) {
+//     const filtered = data.filter((movie) => movie.Genre.includes(genre));
+//     setFilteredList(filtered);
+//   }
+
+//   return (
+//     <>
+//       <h1>Genre</h1>
+//       <p>Select a Genre</p>
+//       <div>
+//         <button
+//           type="button"
+//           className="btn btn-primary"
+//           onClick={() => handleGenre("comedies")}
+//         >
+//           Comedy
+//         </button>
+//         <button
+//           type="button"
+//           className="btn btn-danger"
+//           onClick={() => handleGenre("drama")}
+//         >
+//           Drama
+//         </button>
+//         <button
+//           type="button"
+//           className="btn btn-success"
+//           onClick={() => handleGenre("action")}
+//         >
+//           Action
+//         </button>
+//         <button
+//           type="button"
+//           className="btn btn-warning"
+//           onClick={() => handleGenre("thriller")}
+//         >
+//           Thriller
+//         </button>
+//       </div>
+
+//       <h2>Movies:</h2>
+//       <ul>
+//         {filteredList.map((movie, index) => (
+//           <li key={index}>{movie.Name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+import { useState } from "react";
 
 export default function Genre({ data }) {
+    const [filteredList, setFilteredList] = useState([]);
 
-    const [filteredList, setFilteredList] = useState([])
-
-    function handleGenre() {
-        console.log("clicked Genre")
-
-        const comedyGenre = []
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].Genre.includes("comedies")) {
-                comedyGenre.push(data[i])
-            }
-        }
-        setFilteredList(comedyGenre)
+    // Filter movies by genre
+    function handleGenre(genre) {
+        const filtered = data.filter((movie) => movie.Genre.includes(genre));
+        setFilteredList(filtered);
+        console.log("Filtered Movies:", filtered);
     }
-
-
-    // for (let i = 0; i < comedyGenre.length; i++) {
-    //     filteredList.push(<>
-    //         <li>{filteredList[i].Name}</li>
-    //     </>)
-    // }
 
     return (
         <>
             <h1>Genre</h1>
             <p>Select a Genre</p>
-
-            <button type="button" className="btn btn-primary" onClick={handleGenre}>Comedy</button>
-            <button type="button" className="btn btn-danger">Drama</button>
-            {/* <button type="button" className="btn btn-success" onClick={handleGenre}>Action</button>
-            <button type="button" className="btn btn-warning">Thriller</button> */}
-
-            <div className="card">
-                <div className="card-body">
-                    <h4 className="card-title">
-                    </h4><p className="card-text"></p><a href="#" className="card-link">Card link</a><a href="#" className="card-link">Another link</a>
-
-                </div>
+            <div style={{ marginBottom: "20px" }}>
+                <button
+                    className="btn btn-primary m-1"
+                    onClick={() => handleGenre("Comedies")}
+                >
+                    Comedy
+                </button>
+                <button
+                    className="btn btn-danger m-1"
+                    onClick={() => handleGenre("Dramas")}
+                >
+                    Drama
+                </button>
+                <button
+                    className="btn btn-success m-1"
+                    onClick={() => handleGenre("Action")}
+                >
+                    Action
+                </button>
+                <button
+                    className="btn btn-warning m-1"
+                    onClick={() => handleGenre("Thriller")}
+                >
+                    Thriller
+                </button>
             </div>
 
-
+            <h2>Movies:</h2>
+            <ul>
+                {filteredList.length === 0 ? (
+                    <li>No movies found</li>
+                ) : (
+                    filteredList.map((movie, index) => (
+                        <li key={index}>{movie.Title}</li>
+                    ))
+                )}
+            </ul>
         </>
-    )
+    );
 }
--- src/pages/Ratings.jsx
+++ src/pages/Ratings.jsx
@@ -1,9 +1,66 @@
-export default function Ratings() {
+import { useState } from "react";
+
+export default function Ratings({ data }) {
+    const [filteredList, setFilteredList] = useState([]);
+
+    // Filter movies by rating
+    function handleRating(rating) {
+        const filtered = data.filter((movie) => movie.Rating.includes(rating));
+        setFilteredList(filtered);
+        console.log("Filtered Movies by Rating:", filtered);
+    }
+
     return (
         <>
             <h1>Ratings</h1>
             <p>Select a Rating</p>
-            
+            <div style={{ marginBottom: "20px" }}>
+                <button
+                    className="btn btn-primary m-1"
+                    onClick={() => handleRating("TV-MA")}
+                >
+                    TV-MA
+                </button>
+                <button
+                    className="btn btn-danger m-1"
+                    onClick={() => handleRating("TV-PG")}
+                >
+                    TV-PG
+                </button>
+                <button
+                    className="btn btn-success m-1"
+                    onClick={() => handleRating("TV-14")}
+                >
+                    TV-14
+                </button>
+                <button
+                    className="btn btn-warning m-1"
+                    onClick={() => handleRating("R")}
+                >
+                    R
+                </button>
+                <button
+                    className="btn btn-info m-1"
+                    onClick={() => handleRating("NR")}
+                >
+                    NR
+                </button>
+                <button
+                    className="btn btn-secondary m-1"
+                    onClick={() => handleRating("TV-G")}
+                >
+                    TV-G
+                </button>
+            </div>
+
+            <h2>Movies:</h2>
+            <ul>
+                {filteredList.length === 0 ? (
+                    <li>No movies found</li>
+                ) : (
+                    filteredList.map((movie) => <li key={movie.id}>{movie.Title}</li>)
+                )}
+            </ul>
         </>
-    )
+    );
 }
