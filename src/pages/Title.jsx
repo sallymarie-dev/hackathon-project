da9e22cd177b88984d77e61740dd8035e3fe5e63
isaacwatson30 <isaacwatson30@gmail.com>
2025-10-20 00:32:57 -0500

    worked on the css and style of the title.css, title.jsx and app.jsx

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import "./Title.css";
+import "../Title.css";
 
 export default function Title({ data }) {
   return (
     <>
       <div className="title-page">
-        <h1>Title</h1>
-        <p>Select a Title</p>
-        <h3>{data[25].Title}</h3>
+        <div className="title-header">
+
+          <img src="/moviecard.jpeg" alt="movie cover" width={300} />
+
+          <h3>{data[25].Title}</h3>
+          <h6> Rating: {data[25].Rating}</h6>
+          <h6>Genre: {data[25].Genre}</h6>
+        </div>
       </div>
     </>
   );
