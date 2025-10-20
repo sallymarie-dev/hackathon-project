88fce98368430ec0c15f3a93b0a19828a2f8e563
isaacwatson30 <isaacwatson30@gmail.com>
2025-10-19 21:53:03 -0500

    added Title.css page

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,9 +1,14 @@
-export default function Title({data}) {
-    return (
-        <>
-            <h1>Title</h1>
-            <p>Select a Title</p>
-            <h3>{data[25].Title}</h3>
-        </>
-    )
+import React from "react";
+import "./Title.css";
+
+export default function Title({ data }) {
+  return (
+    <>
+      <div className="title-page">
+        <h1>Title</h1>
+        <p>Select a Title</p>
+        <h3>{data[25].Title}</h3>
+      </div>
+    </>
+  );
 }
