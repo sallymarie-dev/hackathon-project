7d55e3702261c69bb6cb1ba6c9ddc47b8b8d69b8
Sally <causeysally@gmail.com>
2025-10-19 19:02:43 -0500

    still working on displaying data

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,8 +1,46 @@
-export default function Genre() {
+import { useState } from "react"
+
+export default function Genre({ data }) {
+
+    const [filteredList, setFilteredList] = useState([])
+
+    function handleGenre() {
+        console.log("clicked Genre")
+
+        const comedyGenre = []
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].Genre.includes("comedies")) {
+                comedyGenre.push(data[i])
+            }
+        }
+        setFilteredList(comedyGenre)
+    }
+
+
+    // for (let i = 0; i < comedyGenre.length; i++) {
+    //     filteredList.push(<>
+    //         <li>{filteredList[i].Name}</li>
+    //     </>)
+    // }
+
     return (
         <>
             <h1>Genre</h1>
             <p>Select a Genre</p>
+
+            <button type="button" className="btn btn-primary" onClick={handleGenre}>Comedy</button>
+            <button type="button" className="btn btn-danger">Drama</button>
+            {/* <button type="button" className="btn btn-success" onClick={handleGenre}>Action</button>
+            <button type="button" className="btn btn-warning">Thriller</button> */}
+
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">
+                    </h4><p className="card-text"></p><a href="#" className="card-link">Card link</a><a href="#" className="card-link">Another link</a>
+
+                </div>
+            </div>
+
 
         </>
     )
--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,9 +1,9 @@
-export default function Title({data}) {
+export default function Title({ data }) {
     return (
         <>
             <h1>Title</h1>
             <p>Select a Title</p>
-            <h3>{data[25].Title}</h3>
+            <h3>{data[5].Title}</h3>
         </>
     )
 }
